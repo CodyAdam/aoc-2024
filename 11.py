9443bf02df9155472e723513ebed13c596f02e30
Cody Adam <cody.ad4@gmail.com>
2024-12-11 11:55:14 +0100

    improve d11

--- 11.py
+++ 11.py
@@ -1,36 +1,45 @@
-import time
 IN = open("in.txt", "r").read()
 
 nums = list(map(int, IN.split()))
 
 print(nums)
 
-length_cache = {}
 
-def step_length(val, count):
-    if count == 0:
-        return 1
+cache = {}
+hit_count = 0
 
-    if (val, count) in length_cache:
-        return length_cache[(val, count)]
 
-    if val == 0:
-        result = step_length(1, count - 1)
-    elif len(str(val)) % 2 == 0:
-        left, right = str(val)[:len(str(val))//2], str(val)[len(str(val))//2:]
-        left_val = int(left)
-        right_val = int(right)
-        result = step_length(left_val, count - 1) + step_length(right_val, count - 1)
-    else:
-        result = step_length(val * 2024, count - 1)
+def step(nums, count):
+    global hit_count
 
-    length_cache[(val, count)] = result
-    return result
+    if len(nums) == 1:
+        if count == 0:
+            return 1
+        val = nums[0]
+        if (val, count) in cache:
+            hit_count += 1
+            return cache[(val, count)]
+        
+        res = 0
+        if val == 0:
+            res = step([1], count - 1)
+        elif len(str(val)) % 2 == 0:
+            left, right = str(val)[:len(str(val))//2], str(val)[len(str(val))//2:]
+            left_val = int(left)
+            right_val = int(right)
+            res = step([left_val, right_val], count - 1)
+        else: 
+            res = step([val * 2024], count - 1)
+        res = cache[(val, count)]
+        return res
+    else: # multiple, just split
+        return sum([step([val], count) for val in nums])
 
-def total_length(nums, count):
-    return sum(step_length(val, count) for val in nums)
 
-s1 = total_length(nums, 25)
-s2 = total_length(nums, 75)
+s1 = step(nums, 25)
+s2 = step(nums, 75)
+
+print("cache hits", hit_count)
 print(s1)
 print(s2)
+
